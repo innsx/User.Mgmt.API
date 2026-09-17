2349607ae6065512f39eb3b0e239d78cafff5f0c
Nick Xiong <k_xiong2000@hotmail.com>
2026-09-17 12:15:32 -0400

    update TestAuthorization controller so we can test endpoint with Role = 'User' or 'HR'

--- User.Mgmt.API/Controllers/TestAuthorizationController.cs
+++ User.Mgmt.API/Controllers/TestAuthorizationController.cs
@@ -4,19 +4,28 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace User.Mgmt.API.Controllers
 {
-    [Authorize(Roles = "Admin")]
+
     [Route("api/[controller]")]
     [ApiController]
     public class TestAuthorizationController : ControllerBase
     {
+        [Authorize(Roles = "Admin")]
         [HttpGet("employees")]
         public IEnumerable<string> GetEmployees()
         {
             return new List<string> { "Jon", "Sue", "Julie" };
         }
 
+        [Authorize(Roles = "User")]
         [HttpGet("employee-by-id")]
         public IActionResult GetEmployeeById()
+        {
+            return Ok("Jon Doe");
+        }
+
+        [Authorize(Roles = "HR")]
+        [HttpGet("get-employee")]
+        public IActionResult GetEmployee()
         {
             return Ok("Jon Doe");
         }
